e227470c86ca211e84c02b96676e2fdf0590acb6
Moncef Gaha <moncefplastin07@gmail.com>
2021-07-30 23:58:10 +0100

    customize rotate degree

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 module.exports = {
   mode: 'jit',
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: 'class', // or 'media' or 'class'
   theme: {
-    extend: {},
+    rotate: {
+      '360': '360deg',
+     }
   },
   variants: {
     extend: {},
