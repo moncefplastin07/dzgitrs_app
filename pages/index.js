d3e4dbfc826b9b029996010ec8273b5d0dd8e6fd
Moncef Gaha <moncefplastin07@gmail.com>
2021-07-31 15:17:57 +0100

    Fix page title and add regions list search

--- pages/index.js
+++ pages/index.js
@@ -6,8 +6,8 @@ import UserColumn from "../components/UserColumn.js";
 import React, { useState } from 'react';
 import Footer from '../components/Footer.js'
 import ToggleDarkModeButton from '../components/ToggleDarkModeButton.js'
-export default function Home(data) {
-  const { countriesList } = data
+import RegionsList from '../components/RegionsList.js'
+export default function Home({regionsList}) {
   const [isDarkMode, setDarkMode] = useState(false);
   const toggleDarkMode = () => {
     if (isDarkMode){
@@ -16,11 +16,12 @@ export default function Home(data) {
       setDarkMode(true)
     }
   };
+  
   return (
   <div className={`${isDarkMode ? 'dark' : ''}`}>
     <div className="dark:bg-black dark:text-white font-mono flex flex-col items-center justify-center min-h-screen py-2">
       <Head>
-        <title>DZGitrs - The most active GitHub users in </title>
+        <title>DZGitrs - The most active GitHub By geographic region </title>
         <meta name="viewport" content="width=device-width"></meta>
         <link rel="icon" href="/favicon.ico" />
       </Head>
@@ -33,18 +34,7 @@ export default function Home(data) {
           </span>
         </h1>
         <ToggleDarkModeButton onClick={toggleDarkMode} isDarkMode={isDarkMode} />
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-        { countriesList.length ? <ul className='text-left grid grid-cols-4 xs:grid-cols-2 xs:text-center gap-4'>
-             
-            {
-              countriesList.map(country=> (
-                <li key={country.country} className={`${country.slug == 'israel' ? 'line-through text-red-800 bg-red-200 border-red-500' : ''} border-green-500 duration-200 ease-in-out	hover:border-l-4 hover:pl-2`}>
-                  <Link href={country.slug !== 'israel' ? country.slug : '#' }><a title={country.slug !== 'israel' ? country.country : 'A country that encourages terrorism' }>{country.country}</a></Link>
-                </li>
-              ))
-            }
-          </ul> :'لا توجد احصائيات'  }
-        </div>
+        <RegionsList allRegionsList={regionsList} />
       </main>
       <Footer/>
     </div>
@@ -54,14 +44,14 @@ export default function Home(data) {
 
 export async function getServerSideProps() {
   const res = await fetch(`https://dzgitrs.herokuapp.com/get_countries`)
-  const countriesList = await res.json()
-  if (!countriesList) {
+  const regionsList = await res.json()
+  if (!regionsList) {
     return {
       notFound: true,
     }
   }
 
   return {
-    props: { countriesList }, // will be passed to the page component as props
+    props: { regionsList }, // will be passed to the page component as props
   }
 }
