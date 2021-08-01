724ba16ee48813f90b169697f36ea4a00fd9dc11
Moncef Gaha <moncefplastin07@gmail.com>
2021-08-01 14:01:54 +0100

    rename to 'ToggleDisplayThemeButton'

--- components/ToggleDarkModeButton.js
+++
@@ -1,7 +0,0 @@
-export default function ToggleDarkModeButton(props) {
-    return (
-        <button onClick={props.onClick} className={`text-4xl duration-200 ease-in-out fixed right-1.5	top-1.5	${props.isDarkMode ? 'rotate-360' : ''}`}>{props.isDarkMode ? '🌝' : '🌚'}</button>
-    )
-  }
-  
-  
