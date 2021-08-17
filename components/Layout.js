a6f6235bd1b1559cb6546b7ffd969d074f7bc3e2
Moncef Gaha <moncefplastin07@gmail.com>
2021-08-17 09:02:20 +0100

    layou page

---
+++ components/Layout.js
@@ -0,0 +1,45 @@
+import React, { Component, ReactNode, useEffect, useState } from "react";
+import Link from "next/link";
+import Head from "next/head";
+import Footer from "./Footer";
+import ToggleDisplayThemeButton from "./ToggleDisplayThemeButton";
+import { textChangeRangeIsUnchanged } from "typescript";
+
+class Layout extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { displayTheme: "light" };
+  }
+  componentDidMount() {
+    this.setState({
+      displayTheme: window.localStorage.displayTheme || "light",
+    });
+  }
+  toggleDisplayTheme() {
+    const newDisplayTheme = this.state.displayTheme == "light"
+      ? "dark"
+      : "light";
+    window.localStorage.setItem("displayTheme", newDisplayTheme);
+    return newDisplayTheme;
+  }
+  render() {
+    return (
+      <div className={`${this.state.displayTheme} `}>
+        <Head>
+          <title>{this.props.title}</title>
+          <meta name="viewport" content="width=device-width"></meta>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <ToggleDisplayThemeButton
+          onClick={() =>
+            this.setState({ displayTheme: this.toggleDisplayTheme() })}
+          displayTheme={this.state.displayTheme}
+        />
+        {this.props.children}
+        <Footer />
+      </div>
+    );
+  }
+}
+
+export default Layout;
