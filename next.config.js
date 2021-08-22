10bd782dab6e42722fd6b0187dde89c13fa0e562
Moncef Gaha <moncefplastin07@gmail.com>
2021-08-22 14:32:28 +0100

    .

--- next.config.js
+++ next.config.js
@@ -1,13 +1,4 @@
 module.exports = {
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ["@svgr/webpack"]
-    });
-
-    return config;
-  },
-    future: {
-      webpack5: false,
-    }
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH
 }
